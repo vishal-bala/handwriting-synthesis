8c382f620127740c6cd0a834468bb18b5dd61a93
Vishal Bala <vishal.bala@automationhero.ai>
2019-11-29 12:47:05 +0100

    Complete requirements.txt + setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+try:  # For pip >= 10
+    from pip._internal.download import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:  # For pip <= 9.0.3
+    from pip.download import PipSession
+    from pip.req import parse_requirements
+
+setup(
+    name='handwriting-synthesis',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Sean Vasquez',
+    author_email='seanjv@mit.edu',
+    maintainer='Vishal Bala',
+    maintainer_email='vishal@vishalbala.com',
+    install_requires=[str(req.req) for req in parse_requirements(
+        'requirements.txt', session=PipSession(),
+    )],
+)
